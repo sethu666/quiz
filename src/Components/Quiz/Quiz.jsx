59812176640494522b4e68304359d97db83ce81b
Manimaran <moglimani@gmail.com>
2025-03-21 12:51:41 +0530

    dynamic options

--- src/Components/Quiz/Quiz.jsx
+++ src/Components/Quiz/Quiz.jsx
@@ -1,31 +1,27 @@
 import React, { useState, useRef } from "react";
-import "./Quiz.css";
+import "./quiz.scss";
 import { data } from "../../assets/data";
 
 const Quiz = () => {
   let [index, setIndex] = useState(0);
-  let [question, setQuestion] = useState(data[index]);
+  const [question, setQuestion] = useState(data[index]);
   let [lock, setLock] = useState(false);
   let [score, setScore] = useState(0);
   let [result, setResult] = useState(false);
 
-  let Option1 = useRef(null);
-  let Option2 = useRef(null);
-  let Option3 = useRef(null);
-  let Option4 = useRef(null);
 
-  let option_array = [Option1, Option2, Option3, Option4];
-
-  const checkAns = (e, ans) => {
-    if (lock === false) {
-      if (question.ans === ans) {
+  const checkAns = (e, ansCode) => {
+    const qid = e.target.dataset.id
+    const correctans = e.target.dataset.correctans
+    const answered = data?.filter(item => item.id == qid)[0].answer === ansCode
+    setLock(true)
+    if (!lock) {
+      if (answered) {
         e.target.classList.add("correct");
-        setLock(true);
-        setScore((prev) => prev + 1);
       } else {
         e.target.classList.add("wrong");
-        setLock(true);
-        option_array[question.ans - 1].current.classList.add("correct");
+        const correctOption = e.target.parentElement.querySelectorAll(`.item-${correctans}`)
+        correctOption[0].classList.add('correct')
       }
     }
   };
@@ -39,11 +35,6 @@ const Quiz = () => {
       setIndex(++index);
       setQuestion(data[index]);
       setLock(false);
-      option_array.map((option) => {
-        option.current.classList.remove("wrong");
-        option.current.classList.remove("correct");
-        return null;
-      });
     }
   };
 
@@ -55,50 +46,37 @@ const Quiz = () => {
     setResult(false);
   }
 
+  const generateOptions = (question) => {
+
+    return (
+      question.options.map((item, index) => <li
+        data-id={question.id}
+        data-oid={item.code}
+        data-correctans={question.answer}
+        className={`item-${item.code}`}
+        key={`option-${question.id}-${index}`}
+        onClick={(e) => {
+          checkAns(e, item.code);
+        }}
+      >
+        {item.name}
+      </li>)
+    )
+  }
+
   return (
     <div className="container">
       <h1>Quiz App</h1>
       <hr />
-      {result ? (
-        <></>
-      ) : (
+      {!result && (
         <>
           <h2>
             {index + 1}. {question.question}
           </h2>
           <ul>
-            <li
-              ref={Option1}
-              onClick={(e) => {
-                checkAns(e, 1);
-              }}
-            >
-              {question.option1}
-            </li>
-            <li
-              ref={Option2}
-              onClick={(e) => {
-                checkAns(e, 2);
-              }}
-            >
-              {question.option2}
-            </li>
-            <li
-              ref={Option3}
-              onClick={(e) => {
-                checkAns(e, 3);
-              }}
-            >
-              {question.option3}
-            </li>
-            <li
-              ref={Option4}
-              onClick={(e) => {
-                checkAns(e, 4);
-              }}
-            >
-              {question.option4}
-            </li>
+            {
+              generateOptions(question)
+            }
           </ul>
           <button onClick={next}>Next</button>
           <div className="index">
@@ -106,11 +84,13 @@ const Quiz = () => {
           </div>
         </>
       )}
-      {result ? <>
-        <h2>You Scored {score} out of {data.length}</h2>
-        <button onClick={reset}>Reset</button>
-      </>:<></>}
-      
+      {result && (
+        <>
+          <h2>You Scored {score} out of {data.length}</h2>
+          <button onClick={reset}>Reset</button>
+        </>
+      )}
+
     </div>
   );
 };
